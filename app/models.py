874757f5c8ac9a182a04605ccd67f80aa1fa6c40
Thiago J. Barbalho <thiagojobson@gmail.com>
2019-11-11 20:04:18 -0300

    Simplicação dos models
    Agora o app possui os models Usuario e Viagem, com um relacionamento de
    um para muitos.

--- app/models.py
+++ app/models.py
@@ -3,33 +3,20 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-viagens = db.Table(
-    'viagens',
-    db.Column('usuario_id', db.Integer, 
-        db.ForeignKey('usuario.id'), primary_key=True),
-    db.Column('viagem_id', db.Integer, 
-        db.ForeignKey('viagem.id'), primary_key=True)
-)
-
-
 class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(80),nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    firebase_id = db.Column(db.String(80), 
+        unique=True, nullable=False)
+    nome = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), 
+        unique=True, nullable=False)
     criado_em = db.Column(db.DateTime, nullable=False)
-    viagens = db.relationship('Viagem', secondary=viagens)
+    viagens = db.relationship('Viagem', backref='usuario', lazy=True)
 
 
 class Viagem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    criado_por = db.Column(db.Integer, 
-        db.ForeignKey('usuario.id'), nullable=False)
     de = db.Column(db.String(100), nullable=False)
     para = db.Column(db.String(100), nullable=False)
     data = db.Column(db.DateTime, nullable=False)
-
-
-class FirebaseID(db.Model):
-    id = db.Column(db.String, primary_key=True)
-    user_id = db.Column(db.Integer,
-        db.ForeignKey('usuario.id'), nullable=False)
+    criado_em = db.Column(db.DateTime, nullable=False)
